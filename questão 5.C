3e6c63e53e5637627f8855ac0c42481296a01717
Beatriz Padrão <103266259+biadpadrao@users.noreply.github.com>
2022-06-12 20:38:57 -0300

    Create questão 5.C

---
+++ questão 5.C
@@ -0,0 +1,24 @@
+/* Escreva  um  programa  que  concatene  o  conteúdo  de  dois  arquivos.  O  conteúdo  dos  dois arquivos deverá ser adicionado em um terceiro arquivo. */
+
+#include <stdio.h>
+
+int main(void) {
+  FILE *pont_arq1;
+  FILE *pont_arq2;
+  FILE *pont_arqmist;
+  char t1, t2;
+  pont_arq1 = fopen("texto1.txt", "r");
+  pont_arq2 = fopen("texto2.txt", "r");
+  pont_arqmist = fopen("texto3.txt", "w");
+  
+  while ((t1 = fgetc(pont_arq1)) != EOF) { 
+    fprintf(pont_arqmist, "%c", t1); 
+  }
+  
+  while ((t2 = fgetc(pont_arq2)) != EOF) {
+    fprintf(pont_arqmist, "%c", t2);
+  }
+  
+  fclose(pont_arqmist);
+  return 0;
+}
